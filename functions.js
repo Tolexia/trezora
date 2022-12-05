6d6ce2b49a67a896d967e37cf4763afda9d94b29
Axel C <axel.croize@hotmail.fr>
2022-12-05 21:56:54 +0100

    Added attemp count + struggling with relative paths once again

--- functions.js
+++ functions.js
@@ -4,6 +4,7 @@ function moveDirection(direction)
         case "N":
             if(parseInt(localStorage.getItem('boatCordY')) - 1 > 1)
             {
+                localStorage.setItem('trycount', localStorage.getItem('trycount')+1);
                 localStorage.setItem('boatCordY', parseInt(localStorage.getItem('boatCordY'))-1);
                 window.location.reload();
             }
@@ -15,6 +16,7 @@ function moveDirection(direction)
         case "S":
             if(parseInt(localStorage.getItem('boatCordY')) + 1 <= 12)
             {
+                localStorage.setItem('trycount', localStorage.getItem('trycount')+1);
                 localStorage.setItem('boatCordY', parseInt(localStorage.getItem('boatCordY')) + 1);
                 window.location.reload();
             }
@@ -26,6 +28,7 @@ function moveDirection(direction)
         case "E":
             if(parseInt(localStorage.getItem('boatCordX'))+1 > 1)
             {
+                localStorage.setItem('trycount', localStorage.getItem('trycount')+1);
                 localStorage.setItem('boatCordX', parseInt(localStorage.getItem('boatCordX'))+1);
                 window.location.reload();
             }
@@ -37,6 +40,7 @@ function moveDirection(direction)
         case "W":
             if(parseInt(localStorage.getItem('boatCordX')) - 1 <= 12)
             {
+                localStorage.setItem('trycount', localStorage.getItem('trycount')+1);
                 localStorage.setItem('boatCordX', parseInt(localStorage.getItem('boatCordX')) - 1);
                 window.location.reload();
             }
@@ -51,33 +55,17 @@ function moveDirection(direction)
     }
 }
 
-function placeBoatAndTreasure()
-{
-    if(localStorage.getItem('boatCordX') == null || localStorage.getItem('boatCordX') == undefined)
-    {
-        localStorage.setItem('boatCordX', 1);
-    }
-    if(localStorage.getItem('boatCordY') == null || localStorage.getItem('boatCordY') == undefined)
-    {
-        localStorage.setItem('boatCordY', 1);
-    }
-    if(localStorage.getItem('treasureCordX') == null || localStorage.getItem('treasureCordX') == undefined)
-    {
-        localStorage.setItem('treasureCordX', Math.floor(Math.random() * 5)+1);
-    }
-    if(localStorage.getItem('treasureCordY') == null || localStorage.getItem('treasureCordY') == undefined)
-    {
-        localStorage.setItem('treasureCordY', Math.floor(Math.random() * 5)+1);
-    }
-}
 
 function newGame()
 {
    for (key in localStorage) 
-   {
+    {
         localStorage.removeItem(key);
-   }
-   placeBoatAndTreasure()
-   window.location.reload();
+    }
+    localStorage.setItem('boatCordX', 1);
+    localStorage.setItem('boatCordY', 1);
+    localStorage.setItem('treasureCordX', Math.floor(Math.random() * 5)+1);
+    localStorage.setItem('treasureCordY', Math.floor(Math.random() * 5)+1);
+    window.location.reload();
 }
 
