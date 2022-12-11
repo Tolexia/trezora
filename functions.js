3ae6c4376fbba75160411ba25958a2ffda7862df
Axel <axelcroizepro@gmail.com>
2022-12-11 18:39:00 +0100

    big enhancement of animations + beggining of new system of IA opponent & xp

--- functions.js
+++ functions.js
@@ -1,6 +1,9 @@
-function moveDirection(direction)
+function moveDirection(direction, boat = null)
 {
-    const boat = document.querySelector('.boat');
+    if(boat == null)
+    {
+        boat = document.querySelector('#boat');
+    }
     boat.classList.remove('boat');
     switch (direction) {
         case "N":
@@ -8,7 +11,7 @@ function moveDirection(direction)
             {
                 localStorage.setItem('trycount', parseInt(localStorage.getItem('trycount'))+1);
                 localStorage.setItem('boatCordY', parseInt(localStorage.getItem('boatCordY'))-1);
-                boat.style = "animation-name:movingNorth;animation-duration: 3s;animation-fill-mode: forwards;filter: drop-shadow(3px 3px 5px var(--dark));";
+                boat.style = "animation-name:movingNorth;animation-duration: 2s;animation-fill-mode: forwards;filter: drop-shadow(3px 3px 5px var(--dark));";
             }
             else
             {
@@ -20,7 +23,7 @@ function moveDirection(direction)
             {
                 localStorage.setItem('trycount', parseInt(localStorage.getItem('trycount'))+1);
                 localStorage.setItem('boatCordY', parseInt(localStorage.getItem('boatCordY')) + 1);
-                boat.style = "animation-name:movingSouth;animation-duration: 3s;animation-fill-mode: forwards;filter: drop-shadow(3px 3px 5px var(--dark));";
+                boat.style = "animation-name:movingSouth;animation-duration: 2s;animation-fill-mode: forwards;filter: drop-shadow(3px 3px 5px var(--dark));";
             }
             else
             {
@@ -32,7 +35,7 @@ function moveDirection(direction)
             {
                 localStorage.setItem('trycount', parseInt(localStorage.getItem('trycount'))+1);
                 localStorage.setItem('boatCordX', parseInt(localStorage.getItem('boatCordX'))+1);
-                boat.style = "animation-name:movingEast;animation-duration: 3s;animation-fill-mode: forwards;filter: drop-shadow(3px 3px 5px var(--dark));";
+                boat.style = "animation-name:movingEast;animation-duration: 2s;animation-fill-mode: forwards;filter: drop-shadow(3px 3px 5px var(--dark));";
             }
             else
             {
@@ -44,7 +47,7 @@ function moveDirection(direction)
             {
                 localStorage.setItem('trycount', parseInt(localStorage.getItem('trycount'))+1);
                 localStorage.setItem('boatCordX', parseInt(localStorage.getItem('boatCordX')) - 1);
-                boat.style = "animation-name:movingWest;animation-duration: 3s;animation-fill-mode: forwards;filter: drop-shadow(3px 3px 5px var(--dark));";
+                boat.style = "animation-name:movingWest;animation-duration: 2s;animation-fill-mode: forwards;filter: drop-shadow(3px 3px 5px var(--dark));";
             }
             else
             {
@@ -65,7 +68,7 @@ function moveDirection(direction)
         // boat.style = "animation : 3s linear myBoat 0s infinite alternate;"
         boat.style = "";
         checkIfWon();
-    }, 3000);
+    }, 2000);
 }
 
 
@@ -73,7 +76,10 @@ function newGame()
 {
    for (key in localStorage) 
     {
-        localStorage.removeItem(key);
+        if(key != "playerXp")
+        {
+            localStorage.removeItem(key);
+        }
     }
     let minX = 3;
     let maxX = 9;
@@ -83,18 +89,42 @@ function newGame()
     let randomY = Math.floor(Math.random()*(maxY-minY+1)+minY);
     localStorage.setItem('boatCordX', 1);
     localStorage.setItem('boatCordY', 1);
+    localStorage.setItem('boat2CordX', 12);
+    localStorage.setItem('boat2CordY', 6);
     localStorage.setItem('trycount', 0);
     localStorage.setItem('treasureCordX', randomX);
     localStorage.setItem('treasureCordY', randomY);
     localStorage.setItem('displayType', "1");
     window.location.reload();
 }
-
+function gainXp()
+{
+    let xp = 0;
+    if(parseInt(localStorage.getItem('trycount')) <= 3)
+    {
+        xp = 250;
+    }
+    else if(parseInt(localStorage.getItem('trycount')) <= 6)
+    {
+        xp = 500;
+    }
+    else if(parseInt(localStorage.getItem('trycount')) <= 9)
+    {
+        xp = 750;
+    }
+    else if(parseInt(localStorage.getItem('trycount')) > 9)
+    {
+        xp = 1000;
+    }
+    return xp;
+}
 function checkIfWon()
 {
     if(localStorage.getItem('boatCordX') == localStorage.getItem('treasureCordX') && localStorage.getItem('boatCordY') == localStorage.getItem('treasureCordY'))
     {
-        alert('Treasure found in '+ localStorage.getItem('trycount')+" attempts");
+        let xp = gainXp();
+        localStorage.setItem('playerXp', parseInt(localStorage.getItem('playerXp')) + xp)
+        alert('Congrats !\nTreasure found in '+ localStorage.getItem('trycount')+" attempts.\n"+xp+" coins won !");
     }
 }
 
