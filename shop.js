4805bca1656780f5217cac18cbca35f768bc8861
Axel C <69471505+Tolexia@users.noreply.github.com>
2023-10-26 14:15:09 +0200

    Opening animation on shop items container

--- shop.js
+++ shop.js
@@ -1,5 +1,18 @@
+window.itemsContainer = document.getElementById('items')
 function showlist(option)
 {
+    let items;
+    if(option == "Buy")
+    {
+        window.itemsContainer.classList.add("open")
+        items = localStorage.getItem('itemsInShop');
+
+    }
+    else
+    {
+
+        items = localStorage.getItem('itemsToSell');
+    }
 
 }
 
