9294d0570418910eb9b7afcb829a88a018ac62f9
Axel C <69471505+Tolexia@users.noreply.github.com>
2023-10-27 14:22:08 +0200

    shop itemslist opening

--- shop.js
+++ shop.js
@@ -1,12 +1,48 @@
 window.itemsContainer = document.getElementById('items')
-function showlist(option)
+window.itemslist = document.getElementById('itemslist')
+function showlist(option, reload = false)
 {
+    window.itemslist.innerHTML = "";
+    if(window.itemsContainer.classList.contains("open") && !reload)
+    {
+        window.itemsContainer.classList.remove("open")
+        window.itemsContainer.classList.add("close")
+        return setTimeout(() => {
+            showlist(option, true)
+        }, 1000);
+    }
     let items;
+    window.itemsContainer.classList.remove("close")
+    window.itemsContainer.classList.add("open")
     if(option == "Buy")
     {
-        window.itemsContainer.classList.add("open")
-        items = localStorage.getItem('itemsInShop');
+        setTimeout(() => {
+            items = JSON.parse(localStorage.getItem('itemsInShop'));
+            for(item of items)
+            {
+                const divItem = document.createElement('div')
+                const titleItem = document.createElement('span')
+                const buyButton = document.createElement('button')
 
+                divItem.classList.add("divItem")
+                if(item.bought == true)
+                {
+                    divItem.classList.add("bought")
+                }
+
+                titleItem.classList.add("titleItem")
+                titleItem.innerText = item.name
+
+                buyButton.classList.add("buyButton")
+                buyButton.type = "button"
+                buyButton.value = "Buy"
+                buyButton.onclick = () => buy(item)
+
+                divItem.appendChild(titleItem)
+                divItem.appendChild(buyButton)
+                window.itemslist.appendChild(divItem)
+            }
+        }, 1000);
     }
     else
     {
