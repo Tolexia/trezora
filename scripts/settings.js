ed687b7fe24baeaf22028ca20dfbe51d1058f35b
Axel <axelcroizepro@gmail.com>
2023-12-25 22:51:50 +0100

    made boat skins container a slider

--- scripts/settings.js
+++ scripts/settings.js
@@ -22,6 +22,13 @@ function displaySkins()
 {
     window.optionsContent.innerHTML = ""
     
+    const chevronLeft = document.createElement('button')
+    chevronLeft.classList.add('chevronLeft')
+    window.optionsContent.appendChild(chevronLeft)
+
+    const skinsDiv = document.createElement('div')
+    skinsDiv.classList.add('skins')
+    window.optionsContent.appendChild(skinsDiv)
     for(let skin of skins)
     {
         const skinContainer = document.createElement('div')
@@ -36,6 +43,7 @@ function displaySkins()
 
         skinLabel.innerText = skin.title
         skinPicture.src = basePath + skin.file
+        skinPicture.draggable = false
         
         selectButton.type = "button"
         selectButton.dataset.skintitle = skin.title
@@ -52,10 +60,69 @@ function displaySkins()
         skinContainer.appendChild(skinLabel)
         skinContainer.appendChild(skinPicture)
         skinContainer.appendChild(selectButton)
-        window.optionsContent.appendChild(skinContainer)
+        skinsDiv.appendChild(skinContainer)
     }
-}
+    const chevronRight = document.createElement('button')
+    chevronRight.classList.add('chevronRight')
+    window.optionsContent.appendChild(chevronRight)
 
+    chevronLeft.onclick = () => sliderArrow("left", skinsDiv)
+    chevronRight.onclick = () => sliderArrow("right", skinsDiv)
+
+    window.isBlocked = false;
+    skinsDiv.addEventListener('mousedown', e => {
+        window.mouseX = e.clientX
+    })
+    skinsDiv.addEventListener('mouseup', e => {
+        if(e.clientX - window.mouseX < 0 && isBlocked == false)
+        {
+            isBlocked = true;
+            sliderArrow("right", skinsDiv)
+        }
+        else if(e.clientX - window.mouseX > 0 && isBlocked == false)
+        {
+            isBlocked = true;
+            sliderArrow("left", skinsDiv)
+        }
+    })
+}
+function sliderArrow(direction, container)
+{
+    const children = container.childNodes
+    let targetted
+    for(child of children)
+    {
+        if(child.offsetLeft <= container.scrollLeft && container.scrollLeft <= (child.offsetLeft+child.clientWidth))
+        {
+            targetted = child
+        }
+    }
+    if(!targetted)
+    {
+        console.error("No current target")
+        window.isBlocked = false;
+        return;
+    }
+    let target
+    if(direction == "left" && targetted != children[0])
+    {
+        target = targetted.previousElementSibling
+    }
+    else if(direction == "right" && targetted != children[children.length-1])
+    {
+        target = targetted.nextElementSibling
+    }
+    if(!target)
+    {
+        console.error("No target found")
+        window.isBlocked = false;
+        return;
+    }
+    target.scrollIntoView({
+        behavior: "smooth"
+    })
+    window.isBlocked = false;
+}
 function chooseSkin(skin){
     console.log("chooseSkin")
     console.log("skin", skin)
